a040a1df180cd2977f838a80436e433717184375
rxmhxser <156319396+rxmhxser@users.noreply.github.com>
2025-01-11 11:07:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Helping Y A A K U", "Watching Y A A K U"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Watching Y A A K U"];
+const statusTypes = ['idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
